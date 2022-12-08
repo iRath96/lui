8df8c991642092407d7cf07b219e4ca046039b44
Alexander Rath <alex@ist.besonders.cool>
2022-12-09 00:05:36 +0100

    it can draw lenses now

--- include/lui/ui.hpp
+++ include/lui/ui.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <lore/lens/LensSchema.h>
+
 struct UI {
+    UI();
+    
     void draw();
 
 private:
     bool showImguiDemo = false;
     bool showImplotDemo = false;
+
+    lore::LensSchema<float> lens;
 };
--- src/ui.cpp
+++ src/ui.cpp
@@ -1,6 +1,179 @@
 #include <lui/ui.hpp>
 #include <imgui.h>
 #include <implot.h>
+#include <lore/io/LensReader.h>
+#include <fstream>
+
+UI::UI() {
+    lore::GlassCatalog::shared.read("../ext/lore/data/glass/schott.glc");
+    lore::GlassCatalog::shared.read("../ext/lore/data/glass/obsolete001.glc");
+    lore::GlassCatalog::shared.read("../ext/lore/data/glass/hoya.glc");
+
+    std::ifstream file("../ext/lore/data/lenses/wideangle.len");
+    lore::io::LensReader reader;
+    auto result = reader.read(file);
+    lens = result.front();
+}
+
+struct ImTransform2 {
+    ImVec2 offset;
+    ImVec2 scale;
+
+    ImVec2 operator()(const ImVec2 &p) const {
+        return ImVec2 {
+            offset.x + p.x * scale.x,
+            offset.y + p.y * scale.y
+        };
+    }
+
+    ImVec2 operator()(float x, float y) const {
+        return (*this)(ImVec2(x, y));
+    }
+};
+
+void drawStop(
+    float aperture,
+    const ImTransform2 &transform
+) {
+    ImDrawList *drawList = ImGui::GetWindowDrawList();
+    for (int sign = -1; sign <= 1; sign += 2) {
+        drawList->AddLine(
+            transform(0, float(sign) * aperture),
+            transform(0, float(sign) * (aperture + 4)),
+            IM_COL32_WHITE
+        );
+        drawList->AddLine(
+            transform(-2, float(sign) * aperture),
+            transform(+2, float(sign) * aperture),
+            IM_COL32_WHITE
+        );
+    }
+    //drawList->AddLine(
+    //    transform(0, -aperture),
+    //    transform(0, +aperture),
+    //    IM_COL32(255, 255, 255, 64)
+    //);
+}
+
+void drawElement(
+    const lore::SurfaceSchema<float> &left,
+    const lore::SurfaceSchema<float> &right,
+    const ImTransform2 &transform
+) {
+    static std::vector<ImVec2> buffer;
+    buffer.clear();
+
+    const float maxAperture = std::max(left.aperture, right.aperture);
+
+    // MARK: - left surface
+    if (left.isFlat()) {
+        buffer.push_back(transform(0, 0));
+        buffer.push_back(transform(0, maxAperture));
+    } else {
+        buffer.push_back(transform(0, 0));
+
+        const float absRadius = std::abs(left.radius);
+
+        ImTransform2 transform2;
+        transform2.offset = transform.offset;
+        transform2.scale = ImVec2 { transform.scale.x * left.radius, transform.scale.y * absRadius };
+
+        const int N = 32;
+        const float angle = std::asin(left.aperture / absRadius);
+        const float step = angle / float(N);
+        for (int i = 1; i < N; i++) {
+            const float a = float(i) * step;
+            buffer.push_back(transform2(1 - std::cos(a), std::sin(a)));
+        }
+
+        const float sag = left.getSag();
+        buffer.push_back(transform(sag, left.aperture));
+        if (left.aperture < maxAperture) {
+            buffer.push_back(transform(sag, maxAperture));
+        }
+    }
+
+    // MARK: - right surface
+    const float sag = left.thickness + right.getSag();
+    buffer.push_back(transform(sag, maxAperture));
+    if (right.isFlat()) {
+        buffer.push_back(transform(sag, 0));
+    } else {
+        if (right.aperture < maxAperture) {
+            buffer.push_back(transform(sag, right.aperture));
+        }
+
+        const float absRadius = std::abs(right.radius);
+
+        ImTransform2 transform2;
+        transform2.offset = transform.offset;
+        transform2.offset.x += left.thickness * transform.scale.x;
+        transform2.scale = ImVec2 { transform.scale.x * right.radius, transform.scale.y * absRadius };
+
+        const int N = 32;
+        const float angle = std::asin(right.aperture / absRadius);
+        const float step = angle / float(N);
+        for (int i = N - 1; i > 0; i--) {
+            const float a = float(i) * step;
+            buffer.push_back(transform2(1 - std::cos(a), std::sin(a)));
+        }
+
+        buffer.push_back(transform2(0, 0));
+    }
+
+    // MARK: - mirror
+    const int N = buffer.size();
+    buffer.reserve(2 * (N - 1));
+
+    for (int i = N - 2; i > 0; i--) {
+        const auto &p = buffer[i];
+        buffer.emplace_back(p.x, 2 * transform.offset.y - p.y);
+    }
+
+    // MARK: - draw
+    ImDrawList *drawList = ImGui::GetWindowDrawList();
+    drawList->AddPolyline(buffer.data(), buffer.size(), IM_COL32_WHITE, ImDrawFlags_Closed, 1);
+}
+
+void drawLens(const lore::LensSchema<float> &lens) {
+    ImGui::Text("Lens: %s", lens.name.c_str());
+
+    float trackLength = 0;
+    float maxAperture = 0;
+    for (int i = 1; i < lens.surfaces.size() - 1; i++) {
+        const auto &s = lens.surfaces[i];
+        trackLength += s.thickness;
+        maxAperture = std::max(maxAperture, s.aperture);
+    }
+
+    ImGui::Text("TTL: %f", trackLength);
+
+    const ImVec2 targetSize = { 600, 400 };
+    const float scale = std::min(targetSize.x / trackLength, targetSize.y / (2 * maxAperture));
+    const ImVec2 actualSize = { trackLength * scale, 2 * maxAperture * scale };
+
+    ImTransform2 transform;
+    transform.scale = { scale, scale };
+    transform.offset = {
+        ImGui::GetCursorScreenPos().x,
+        ImGui::GetCursorScreenPos().y + transform.scale.y * maxAperture
+    };
+
+    float z = 0;
+    for (int i = 1; i < lens.surfaces.size() - 1; i++) {
+        const auto &surface = lens.surfaces[i];
+        if (i == lens.stopIndex) {
+            drawStop(surface.aperture, transform);
+        }
+        if (!surface.glass.isAir()) {
+            drawElement(surface, lens.surfaces[i + 1], transform);
+        }
+        transform.offset.x += transform.scale.x * surface.thickness;
+    }
+    ImGui::Dummy(actualSize);
+
+    ImGui::Text("z = %f", z);
+}
 
 void UI::draw() {
     if (ImGui::BeginMainMenuBar()) {
@@ -18,6 +191,7 @@ void UI::draw() {
 
     if (ImGui::Begin("lui")) {
         ImGui::Text("Hello");
+        drawLens(lens);
         ImGui::End();
     }
 }
